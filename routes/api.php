6aeabd98b1dd90ba11f384dbd445e83c56d348eb
Ben Ouattara <eshe@live.fr>
2020-07-14 00:56:40 +0000

    :building_construction: add some controllers

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/', function () {
+    $password = Hash::make('password');
+    return response()->json($password);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
