29b80b3eafbccadd3729fff5c9f3fd1a3c1e4cc1
Fitz-G <fitzgeraldjw04@gmail.com>
2020-08-02 00:43:19 +0000

    Update form

--- database/seeds/MasseSeeder.php
+++ database/seeds/MasseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MasseSeeder extends Seeder
 {
@@ -11,6 +12,12 @@ class MasseSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('masses')->insert([
+            'parish_id' => 1,
+            'name' => 'Messe de 7h30',
+            'start_at' => '07:30',
+            'request_closed_at' => '06:00',
+            'start_day' => 2,
+        ]);
     }
 }
--- public/js/customVue.js
+++ public/js/customVue.js
@@ -1,11 +1,19 @@
-$(function () {
-    $('#select2-2').on('change', async function  ()  {
+$('document').ready(function () {
+    $('#select2-2').on('change', async function () {
         const parish_id = parseInt($(this).val());
-        let response = await fetch(`http://d2messe.test/api/parishes/${parish_id}/masses`);
-        if (response.ok){
+        let response = await fetch(`http://d2masse.test/api/parishes/${parish_id}/masses`);
+        if (response.ok) {
             const massesList = await response.json();
-            console.log(massesList);
-        }else {
+            console.log(massesList.length);
+            if (massesList) {
+                for (let i = 0; i < massesList.length; i++) {
+                    const item = massesList[i]
+                    $('#masse_id').append(
+                        "<option value=" + item.id + ">" + item.nom + "</option>"
+                    )
+                }
+            }
+        } else {
 
         }
     });
