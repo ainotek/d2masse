1d121bf3842d2c05303983892ac42fe9e04051d9
Ben Ouattara <eshe@live.fr>
2020-08-07 20:53:41 +0000

    up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -532,7 +532,7 @@
     <div class="modal-dialog modal-lg mt-0" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">{{_('Faire une demande de messe')}}</h5>
+                <h5 class="modal-title" id="exampleModalLabel">{{__('Faire une demande de messe')}}</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
